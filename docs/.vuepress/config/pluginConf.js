ed0f2ef2960d813f63140b6e4c9b3c4432b0b80f
zhangshuai <zhangshuai@opssino.com>
2020-12-15 20:13:33 +0800

    添加titleMap文件和back-to-top功能

--- docs/.vuepress/config/pluginConf.js
+++ docs/.vuepress/config/pluginConf.js
@@ -1,6 +1,9 @@
+const titleMap = require('./titleMap');
 module.exports = {
   "vuepress-plugin-auto-sidebar": {
     titleMode: "uppercase",
-    sidebarDepth: 2
-  }
+    sidebarDepth: 2,
+    titleMap: titleMap
+  },
+  "@vuepress/plugin-back-to-top": true
 };
---
+++ docs/.vuepress/config/titleMap.js
@@ -0,0 +1,5 @@
+module.exports = { 
+  //目录名称       映射的中文名称
+  projectManage: "项目管理",
+  operationManual: "使用手册"
+}
